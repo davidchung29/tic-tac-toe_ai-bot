9c58fb7d34e975381fc262ee4398301db4ea55c8
davidchung29 <60008214+dvjr2018@users.noreply.github.com>
2021-08-05 08:38:59 -0700

    added tic tac toe grid and method to check if game is finished

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 #Create working tic tac toe
+gui_board = [['_','_','_'],
+             ['_','_','_'],
+             ['_','_','_']]
+o_turn = True
+
+
+def game_ends(board):
+
+  for r in board:
+    if len(set(r)) == 1:
+      if r[0] != '_':
+        return True, f'{r[0]} is winner'
+      else: 
+        return False, ''
+  if board[0][0] == board[1][1] == board[2][2]:
+    if r[0] != '_':
+      return True, f'{board[0][0]} is winner'
+    else: 
+      return False, ''
+  elif board[2][0] == board[1][1] == board[0][2]:
+    if r[0] != '_':
+      return True, f'{board[0][2]} is winner'
+    else:
+      return False, ''
+  else:
+    return False, ''
 
 
 
